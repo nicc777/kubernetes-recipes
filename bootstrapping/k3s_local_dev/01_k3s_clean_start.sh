64e28bd6b47228c5cc937ca3f97532aef456138f
Nico Coetzee <nicc777@gmail.com>
2025-08-13 08:16:32 +0200

    doc: [#7] Working on install script

--- bootstrapping/k3s_local_dev/01_k3s_clean_start.sh
+++ bootstrapping/k3s_local_dev/01_k3s_clean_start.sh
@@ -21,26 +21,66 @@ ask() {
 }
 
 function check_command() {
-  if ! command -v "$1" >/dev/null 2>&1; then
-    echo "$1 could not be found"
+  # if ! command -v "$1" >/dev/null 2>&1; then
+  #   echo "$1 could not be found"
+  #   exit 1
+  # fi
+
+  if type "$1" >/dev/null 2>&1; then
+    echo "Command '$1' found..."
+  else
+    echo "The command '$1' does not exist. Cannot continue!"
     exit 1
   fi
+
 }
 
 function uninstall_k3s() {
-  check_command "helm"
-  check_command "curl"
-  /usr/local/bin/k3s-uninstall.sh || true
+  check_command "kubectl"
+  check_command "ssh"
+  ssh $SERVER /usr/local/bin/k3s-uninstall.sh
   echo "[INFO] K3s uninstalled"
 }
 
 function install_k3s() {
-  helm repo update
-  curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s - --disable=traefik --kubelet-arg="node-ip=0.0.0.0"
-  sudo cp -vf /etc/rancher/k3s/k3s.yaml ./k3s.yaml
-  sudo chown $USER:$USER ~/k3s.yaml
+  cat <<EOF >/tmp/k3s_install.sh
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s - --disable=traefik --kubelet-arg="node-ip=0.0.0.0"
+sudo cp -vf /etc/rancher/k3s/k3s.yaml /tmp/k3s.yaml
+sudo chown $USER:$USER /tmp/k3s.yaml
+EOF
+
+  scp /tmp/k3s_install.sh $SERVER:/tmp/
+
+  ssh $SERVER "chmod 700 /tmp/k3s_install.sh && sudo /tmp/k3s_install.sh"
   echo "[INFO] K3s installed"
 }
+
+function get_kubernetes_config() {
+  scp $SERVER:/tmp/k3s.yaml ~/
+  sed -i "s/127.0.0.1/${SERVER}/g" $HOME/k3s.yaml
+}
+
+function validate_phase_1() {
+  kubectl get namespaces >/dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    echo "✅ Successfully connected to the Kubernetes cluster."
+  else
+    echo "❌ Error: Could not connect to the Kubernetes cluster."
+    echo "Please check your kubectl configuration and network connectivity."
+    exit 1
+  fi
+}
+
+REQUIRED_VARS=("SERVER" "LE_R53_KEY" "LE_R53_SECRET" "ROUTE_53_ZONEID" "ROUTE_53_DOMAIN" "ROUTE_53_RESOURCE_ID" "KUBECONFIG" "NFS_SERVER" "NFS_PATH" "EMAIL" "ROUTES")
+
+# Loop through each variable name in the list
+for var in "${REQUIRED_VARS[@]}"; do
+  # Check if the variable is set and not empty
+  if [ -z "${!var}" ]; then
+    echo "Error: Required environment variable '$var' is not set." >&2
+    exit 1
+  fi
+done
 
 echo
 echo "!!! WARNING !!!"
@@ -54,6 +94,8 @@ echo
 if "$answer"; then
   uninstall_k3s
   install_k3s
+  get_kubernetes_config
+  validate_phase_1
 else
   echo "Ok - not doing anything!"
 fi
