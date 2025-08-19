386e7d5546171668ba1b9db7c376458eb6921246
Nico Coetzee <nicc777@gmail.com>
2025-08-19 12:36:26 +0200

    feat: [#3] creating initial validating hook template and github action to build the public container image

--- admission_webhooks/src/launcher.sh
+++ admission_webhooks/src/launcher.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-WARNING_MESSAGE="no warnings from launcher..."
+cd /code/app
 
 if [ "$USE_TLS" = "0" ]; then
-  WARNING_MESSAGE="WARNING: the application has started without the use of TLS termination"
+  echo "WARNING: the application has started without the use of TLS termination"
   uvicorn validation_hook:app --host $HOST --port $PORT --workers $WORKERS
 else
+  echo "no warnings from launcher..."
   uvicorn validation_hook:app --host $HOST --port $PORT --workers $WORKERS --ssl-keyfile=$SSL_KEY_FILE --ssl-certfile=$SSL_CERTIFICATE_FILE
 fi
