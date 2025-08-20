0cc754820215bf73c876c80100a65d075ee5f0e1
Nico Coetzee <nicc777@gmail.com>
2025-08-20 06:47:44 +0200

    fix: [#3] fixed container application references

--- admission_webhooks/src/launcher.sh
+++ admission_webhooks/src/launcher.sh
@@ -4,8 +4,8 @@ cd /code/app
 
 if [ "$USE_TLS" = "0" ]; then
   echo "WARNING: the application has started without the use of TLS termination"
-  uvicorn validation_hook:app --host $HOST --port $PORT --workers $WORKERS
+  uvicorn ${APP_FILE}:app --host $HOST --port $PORT --workers $WORKERS
 else
   echo "no warnings from launcher..."
-  uvicorn validation_hook:app --host $HOST --port $PORT --workers $WORKERS --ssl-keyfile=$SSL_KEY_FILE --ssl-certfile=$SSL_CERTIFICATE_FILE
+  uvicorn ${APP_FILE}:app --host $HOST --port $PORT --workers $WORKERS --ssl-keyfile=$SSL_KEY_FILE --ssl-certfile=$SSL_CERTIFICATE_FILE
 fi
